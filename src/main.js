537cde5de84ad8eba9c02b0efd94c634c9810130
Simon <kartsims@gmail.com>
2016-08-24 11:48:24 +0200

    cordova config

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from './App'
 
 import VueCordova from 'vue-cordova'
 Vue.use(VueCordova, {
-  option1: 'test'
+  optionTestKey: 'optionTestValue'
 })
-
-console.log('main', Vue.cordova)
 
 /* eslint-disable no-new */
 new Vue({
